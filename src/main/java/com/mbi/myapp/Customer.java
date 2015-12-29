f746284d468c65293e49a0a4adf19ce26b367ca3
nianga <nianga@montrusco.com>
2015-12-29 13:44:58 -0500

    Refactored getCustomers and renamed to getCustomersList

---
+++ src/main/java/com/mbi/myapp/Customer.java
@@ -0,0 +1,18 @@
+package com.mbi.myapp;
+
+public class Customer {
+String FirstName;
+String LastName;
+public String getFirstName() {
+	return FirstName;
+}
+public void setFirstName(String firstName) {
+	FirstName = firstName;
+}
+public String getLastName() {
+	return LastName;
+}
+public void setLastName(String lastName) {
+	LastName = lastName;
+}
+}
---
+++ src/main/java/com/mbi/myapp/getCustomersList.java
@@ -0,0 +1,12 @@
+package com.mbi.myapp;
+
+import java.util.List;
+
+public class getCustomersList implements IdataAccess {
+
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
